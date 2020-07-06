996d26d31b7c1e3ae6836d157a59e1f15cbef822
discussion4all <discussion4all@yahoo.com>
2020-07-06 17:35:41 +0530

    phone validation

--- src/pages/PhoneEntry.js
+++ src/pages/PhoneEntry.js
@@ -4,11 +4,34 @@ import dummy from "../images/vs-img.jpg";
 
 const PhoneEntry = (props) => {
   const [showSample, setShowSample] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   const { title, description, blogPosterURL, sampleText } = props.newsletter;
 
   const handleSignUp = () => {
     props.history.push("/phone-verification");
+  };
+
+  const handlePhoneNumber = (e) => {
+    const value =
+      e.target.value.match(/[0-9]/g) && e.target.value.match(/[0-9]/g).join("");
+    console.log("value", value);
+    let formated;
+    if (value && value.length >= 7) {
+      formated = `(${value.slice(0, 3)}) ${value.slice(3, 6)}-${value.slice(
+        6
+      )}`;
+      // formated = `(${value.slice(0, 3)}) ${value.slice(3)}`;
+    } else if (value && value.length >= 4) {
+      // formated = `(${value.slice(0, 3)}) ${value.slice(3, 6)}-${value.slice(
+      //   6
+      // )}`;
+      formated = `(${value.slice(0, 3)}) ${value.slice(3)}`;
+    } else {
+      formated = value;
+    }
+
+    setPhoneNumber(formated || "");
   };
 
   return (
@@ -31,18 +54,27 @@ const PhoneEntry = (props) => {
           </span>
         </h2>
         <div className="select-mod">
-          <div className="flex">
-            <div className="mod-flx">
-              <select>
-                <option>+1</option>
-                <option>+91</option>
-              </select>
-              <input type="text" name="text" placeholder="(555) 555-5555" />
+          <form onSubmit={handleSignUp}>
+            <div className="flex">
+              <div className="mod-flx">
+                <select>
+                  <option>+1</option>
+                  <option>+91</option>
+                </select>
+                <input
+                  type="text"
+                  name="text"
+                  value={phoneNumber}
+                  placeholder="(555) 555-5555"
+                  maxLength="14"
+                  minLength="14"
+                  onChange={handlePhoneNumber}
+                />
+              </div>
+              <button type="submit">Sign Up</button>
             </div>
-            <button type="button" onClick={handleSignUp}>
-              Sign Up
-            </button>
-          </div>
+          </form>
+
           <a
             href="#"
             className="view-btn"
